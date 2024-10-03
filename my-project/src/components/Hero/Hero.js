5d0df653d62b5cddaef9681ea9618073b34e4663
Erick Fujita <erickfujitao@gmail.com>
2024-10-03 01:20:04 -0300

    f

--- my-project/src/components/Hero/Hero.js
+++ my-project/src/components/Hero/Hero.js
@@ -12,11 +12,11 @@ const Hero = (props) => (
           Olá!
         </SectionTitle>
         <SectionText>
-        Eu sou o Erick Fujita, mais conhecido como <span className='fujita'>Fujita</span>. Sou estudante de Engenharia de Software na <span class='text-red-200'>FIAP</span>. Comecei cedo no mundo tecnológico e do marketing, comecei cedo com uma empresa responsável por vender e controlar logísticas de produtos da <a href='https://cloudybr.com.br/' target='blank'>Cloudy</a>.
+        Eu sou o <span className='text-blue-900 font-semibold'>Erick Fujita</span>, mais conhecido como Fujita. Sou estudante de Engenharia de Software na <span class='text-red-400 font-semibold'>FIAP</span>. Comecei cedo no mundo tecnológico e do marketing, comecei cedo com uma empresa responsável por vender e controlar logísticas de produtos da <a href='https://cloudybr.com.br/' target='blank' className='text-blue-300'>Cloudy</a>.
         </SectionText>
         <Button onClick={(e) => {
           e.preventDefault();
-          window.location.href = '/files/resume.pdf';
+          window.location.href = '../../../assets/files/fujita.pdf';
         }}>Acesse aqui</Button>
       </LeftSection>
     </Section>
--- my-project/src/pages/_app.js
+++ my-project/src/pages/_app.js
@@ -1,5 +1,6 @@
 import Theme from '../styles/theme';
 import Head from 'next/head'
+import '../styles/main.css';
 
 export default function App({ Component, pageProps }) {
   return (
